9531365096cc62e776284e8b71455538fa52159b
RiseYagamiChan <131196240+RiseYagamiChan@users.noreply.github.com>
2024-01-11 01:01:56 +0700

    Update _ayra.py

--- modules/_ayra.py
+++ modules/_ayra.py
@@ -10,18 +10,18 @@ from . import LOG_CHANNEL, Button, asst, ayra_cmd, eor, get_string
 
 REPOMSG = """
 ◈ **ᴀʏʀᴀ ꭙ ᴜꜱᴇʀʙᴏᴛ​** ◈\n
-◈ Repo - [Click Here](https://github.com/naya1503/Naya-Userbot)
-◈ Support - @kynansupport
+◈ Repo - [Click Here](https://github.com/RiseYagamiChan/Wynne-Userbot)
+◈ Support - @WynneSupports
 """
 
 RP_BUTTONS = [
     [
-        Button.url(get_string("bot_3"), "https://github.com/naya1503/Naya-Userbot"),
+        Button.url(get_string("bot_3"), "https://github.com/RiseYagamiChan/Wynne-Userbot"),
     ],
-    [Button.url("Support Group", "t.me/kynansupport")],
+    [Button.url("Support Group", "t.me/WynneSupports")],
 ]
 
-AYSTRING = """🎇 **Thanks for Deploying Naya-Userbot**
+AYSTRING = """🎇 **Thanks for Deploying Wynne-Userbot**
 
 • Here, are the Some Basic stuff from, where you can Know, about its Usage."""
 
@@ -32,7 +32,7 @@ async def useAyra(rs):
     msg = await asst.send_message(
         rs.chat_id,
         AYSTRING,
-        file="https://graph.org/file/60408fea8439e6702674d.jpg",
+        file="https://graph.org/file/9d23de7abf1b29d19dd5c.jpg",
         buttons=button,
     )
     if not (rs.chat_id == LOG_CHANNEL and rs.client._bot):
